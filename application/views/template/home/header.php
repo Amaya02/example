bcb3cdfbdc851df1fa1b7d60e850c28f7c0e3203
elishabuuu <elishaevangelistaa@gmail.com>
2018-11-22 18:55:25 +0800

    signup

--- application/views/template/home/header.php
+++ application/views/template/home/header.php
@@ -6,9 +6,14 @@
     <meta name="description" content="" />
     <meta name="author" content="">
 
-    <title>Example</title>
+    <title>Genque3</title>
 
-    <link href="<?php echo base_url('assets/css/bootstrap.css'); ?>" rel="stylesheet" />
-
+    
+    <link  rel="stylesheet" href="<?php echo base_url('assets/css/signup.css');?>" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/css//bootstrap.css'); ?>"/>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" />
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
